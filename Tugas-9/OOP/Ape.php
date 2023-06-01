54c9d1990c06bba0fb3d8440f0b0b9f13ced632a
AisyaAnwarPutri <aisyanwarputri@gmail.com>
2023-06-02 01:11:17 +0700

    Tugas9-OOP

---
+++ Tugas-9/OOP/Ape.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once './animal.php';
+
+class ape extends animal
+{
+	public $jump = "jump: Hop Hop";
+
+	public function jump()
+	{
+		return $this->jump;
+	}
+}
---
+++ Tugas-9/OOP/animal.php
@@ -0,0 +1,27 @@
+<?php
+class animal
+{
+	public $name;
+	public $legs = "Legs : 4";
+	public $cold_blooded = "Cold Blooded : No";
+
+	public function __construct($name)
+	{
+		$this->name = $name;
+	}
+
+	public function get_name()
+	{
+		return $this->name;
+	}
+
+	public function get_legs()
+	{
+		return $this->legs;
+	}
+
+	public function cold_blooded()
+	{
+		return $this->cold_blooded;
+	}
+}
